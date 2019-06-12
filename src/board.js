a291d75e5833baf233310b7e73da1d592063d191
Kate Sowles <katesowles@gmail.com>
2019-06-11 17:47:16 -0700

    moving styled comps outside of function components

--- src/board.js
+++ src/board.js
@@ -1,71 +1,60 @@
-import React from 'react';
-import styled from 'styled-components'; 
+import React from "react";
+import styled from "styled-components";
 
-import Square from './square.js';
+import Square from "./square.js";
 
-export default function Board (props) {
+const StyledRow = styled.div`
+  &:after {
+    clear: both;
+    content: "";
+    display: table;
+  }
+`;
+
+export default function Board(props) {
   const { combo, squares, onClick } = props;
 
-  function renderSquare (rowIndex, colIndex) {
-    const i = (3 * colIndex) + rowIndex;
+  function renderSquare(rowIndex, colIndex) {
+    const i = 3 * colIndex + rowIndex;
 
     return (
       <Square
-      key={ i }
-      value={ squares[i] }
-      onClick={ () => onClick(i) }
-      // if current move matches any of winning combo indexes, pass winningCombo boolean to square for highlighting
-      winningCombo={ combo && (i === combo[0] || i === combo[1] || i === combo[2]) } />
-    )
-	};
-	
-	const Row = ({children, ...props}) => {
-		const {
-			rowIndex
-		} = props;
-
-		const StyledRow = styled.div`
-			&:after {
-				clear: both;
-				content: "";
-				display: table;
-			}
-		`;
-
-		return (
-			<StyledRow>
-				{ children }
-			</StyledRow>
-		);
-	}
-
-  function renderColumns (rowIndex) {
-    let arr = [];
-    
-    for(let c = 0; c < 3; c++) {
-      arr.push( renderSquare(c, rowIndex) );
-    }
-
-    return (
-      <Row key={ rowIndex } >
-        {arr}
-      </Row>
+        key={i}
+        value={squares[i]}
+        onClick={() => onClick(i)}
+        // if current move matches any of winning combo indexes, pass winningCombo boolean to square for highlighting
+        winningCombo={
+          combo && (i === combo[0] || i === combo[1] || i === combo[2])
+        }
+      />
     );
   }
 
-  function renderRows () {
+  const Row = ({ children, ...props }) => {
+    const { rowIndex } = props;
+
+    return <StyledRow>{children}</StyledRow>;
+  };
+
+  function renderColumns(rowIndex) {
     let arr = [];
 
-    for(let r = 0; r < 3; r++) {
-      arr.push( renderColumns(r) );
+    for (let c = 0; c < 3; c++) {
+      arr.push(renderSquare(c, rowIndex));
+    }
+
+    return <Row key={rowIndex}>{arr}</Row>;
+  }
+
+  function renderRows() {
+    let arr = [];
+
+    for (let r = 0; r < 3; r++) {
+      arr.push(renderColumns(r));
     }
 
     return arr;
   }
 
-  return (
-    <div>
-      { renderRows() }
-    </div>
-  );
-} 
+  return <div>{renderRows()}</div>;
+}
--- src/square.js
+++ src/square.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import styled from 'styled-components'; 
+import React from "react";
+import styled from "styled-components";
+
+const StyledSquare = styled.button`
+  background: #fff;
+  background-color: ${props => (props.winningCombo ? "#eee" : "transparent")};
+  border: 1px solid #999;
+  float: left;
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 34px;
+  height: 34px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  width: 34px;
+  &:focus {
+    outline: none;
+    background: #ddd;
+  }
+`;
 
 export const Square = props => {
-	const {
-		onClick,
-		winningCombo,
-		value
-	} = props;
+  const { onClick, winningCombo, value } = props;
 
-	const StyledSquare = styled.button`
-		background: #fff;
-		background-color: ${props => props.winningCombo ? '#eee' : 'transparent'};
-		border: 1px solid #999;
-		float: left;
-		font-size: 24px;
-		font-weight: bold;
-		line-height: 34px;
-		height: 34px;
-		margin-right: -1px;
-		margin-top: -1px;
-		padding: 0;
-		text-align: center;
-		width: 34px;
-		&:focus {
-			outline: none;
-			background: #ddd;
-		}
-	`;
-
-	return (
-		<StyledSquare winningCombo={ winningCombo } className="square" onClick={ onClick }>
-			{ value }
-		</StyledSquare>	);
+  return (
+    <StyledSquare
+      winningCombo={winningCombo}
+      className="square"
+      onClick={onClick}
+    >
+      {value}
+    </StyledSquare>
+  );
 };
 
 export default Square;
